da9c38024bb9d6d24055ff66cb4fc407ada95c73
PriyomDatta <priyomdat@gmail.com>
2025-05-05 05:22:32 +0000

    One of the way to test.

--- test/Module_1/test/test_own_impl.c
+++ test/Module_1/test/test_own_impl.c
@@ -17,32 +17,32 @@ void get_intf_1(char *var_2) {
 // Test function for process_1
 void test_process_1() {
     // Test case 1: var_2 = '+'
-    char var_2 = '+';
-    get_intf_1(&var_2);
+    //char var_2 = '+';
+    //get_intf_1(&var_2);
     int result = process_1();
     assert(result == 12); // 10 + 2 = 12
 
     // Test case 2: var_2 = '-'
-    var_2 = '-';
-    get_intf_1(&var_2);
+    //var_2 = '-';
+    //get_intf_1(&var_2);
     result = process_1();
     assert(result == 8); // 10 - 2 = 8
 
     // Test case 3: var_2 = '*'
-    var_2 = '*';
-    get_intf_1(&var_2);
+    //var_2 = '*';
+    //get_intf_1(&var_2);
     result = process_1();
     assert(result == 20); // 10 * 2 = 20
 
     // Test case 4: var_2 = '/'
-    var_2 = '/';
-    get_intf_1(&var_2);
+    //var_2 = '/';
+    //get_intf_1(&var_2);
     result = process_1();
     assert(result == 5); // 10 / 2 = 5
 
     // Test case 5: var_2 = 'x' (default case)
-    var_2 = 'x';
-    get_intf_1(&var_2);
+    //var_2 = 'x';
+    //get_intf_1(&var_2);
     result = process_1();
     assert(result == 10); // No operation, result remains 10
 
